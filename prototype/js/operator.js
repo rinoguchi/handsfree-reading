f81d0000c64e765ff19a4c4312448429248a091b
井口　良一 <r-inoguchi@m3.com>
2014-10-20 16:23:59 +0900

    change commands to sections

--- prototype/js/operator.js
+++ prototype/js/operator.js
@@ -1,27 +1,27 @@
 // chrome extension化した際に、option化の予定
-var commands = [
-  {name:'command_0', fnc:function(){return '材料を読み上げる関数';}},
-  {name:'command_1', fnc:function(){return 'レシピの項１を読み上げる関数';}},
-  {name:'command_2', fnc:function(){return 'レシピの項２を読み上げる関数';}},
-  {name:'command_3', fnc:function(){return 'レシピの項３を読み上げる関数';}},
-  {name:'command_4', fnc:function(){return 'レシピの項４を読み上げる関数';}},
-  {name:'command_5', fnc:function(){return 'レシピの項５を読み上げる関数';}},
-  {name:'command_6', fnc:function(){return 'レシピの項６を読み上げる関数';}},
-  {name:'command_7', fnc:function(){return 'レシピの項７を読み上げる関数';}},
-  {name:'command_8', fnc:function(){return 'レシピの項８を読み上げる関数';}},
-  {name:'command_9', fnc:function(){return 'レシピの項９を読み上げる関数';}}];
+var sections = [
+  {name:'zairyo', fnc:function(){return '材料を読み上げる関数';}},
+  {name:'1', fnc:function(){return 'レシピの項１を読み上げる関数';}},
+  {name:'2', fnc:function(){return 'レシピの項２を読み上げる関数';}},
+  {name:'3', fnc:function(){return 'レシピの項３を読み上げる関数';}},
+  {name:'4', fnc:function(){return 'レシピの項４を読み上げる関数';}},
+  {name:'5', fnc:function(){return 'レシピの項５を読み上げる関数';}},
+  {name:'6', fnc:function(){return 'レシピの項６を読み上げる関数';}},
+  {name:'7', fnc:function(){return 'レシピの項７を読み上げる関数';}},
+  {name:'8', fnc:function(){return 'レシピの項８を読み上げる関数';}},
+  {name:'9', fnc:function(){return 'レシピの項９を読み上げる関数';}}];
 
 // 以下はchrome extension化しても変更無し
 var Operator = function() {
   var self = this;
 
   // セクションを読み上げ
-  // @param commandName : 'command_0'〜'command_9'のいずれかの値
-  self.readSection = function(commandName) {
+  // @param sectionName : sections.nameの値
+  self.readSection = function(sectionName) {
     var text = '指示内容が定義されていません。';
-    jQuery.each(commands, function(i, command) {
-      if(command.name == commandName) {
-        text = command.fnc();
+    jQuery.each(sections, function(i, section) {
+      if(section.name == sectionName) {
+        text = section.fnc();
         return false;
       }
     });
