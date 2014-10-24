b700d2b8ff35cf0cdcf92bc1579f6626d557324a
井口　良一 <r-inoguchi@m3.com>
2014-10-24 11:22:55 +0900

    add demo site setting

--- chrome_extension/js/common.js
+++ chrome_extension/js/common.js
@@ -11,7 +11,7 @@ window.HandsFree = window.HandsFree || (function() {
 			direction: 'horizontal',
 			siteSettings: [{
 					name: 'CookPad',
-					url: 'http://cookpad.com/.*',
+					url: 'http.*://cookpad.com/.*',
 					sections: [
 						{ name: 'zairyo', func: 'function() {return Array.prototype.slice.call(document.getElementsByClassName("ingredient")).map(function(e) { var nElms = e.getElementsByClassName("ingredient_name"); var qElms = e.getElementsByClassName("ingredient_quantity"); return (nElms[0] ? nElms[0].innerText : "") + "。" + (qElms[0] ? qElms[0].innerText : "");}).join("。");}' },
 						{ name: '1', func: 'function() {return document.getElementsByClassName("step_text")[0].innerText}' },
@@ -23,6 +23,22 @@ window.HandsFree = window.HandsFree || (function() {
 						{ name: '7', func: 'function() {return document.getElementsByClassName("step_text")[6].innerText}' },
 						{ name: '8', func: 'function() {return document.getElementsByClassName("step_text")[7].innerText}' },
 						{ name: '9', func: 'function() {return document.getElementsByClassName("step_text")[8].innerText}' }
+					]
+				},
+				{
+					name: 'Demo',
+					url: 'http.*://demo.com/.*',
+					sections: [
+						{ name: '0', func: 'function() {return "0";}' },
+						{ name: '1', func: 'function() {return "1";}' },
+						{ name: '2', func: 'function() {return "2";}' },
+						{ name: '3', func: 'function() {return "3";}' },
+						{ name: '4', func: 'function() {return "4";}' },
+						{ name: '5', func: 'function() {return "5";}' },
+						{ name: '6', func: 'function() {return "6";}' },
+						{ name: '7', func: 'function() {return "7";}' },
+						{ name: '8', func: 'function() {return "8";}' },
+						{ name: '9', func: 'function() {return "9";}' }
 					]
 				}
 			]
